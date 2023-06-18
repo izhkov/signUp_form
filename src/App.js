3e0f980fe11d99ec4d4572e7dda7f9515f38ae17
Ivan Izhkov <ivan.izhkov@mail.ru>
2023-06-18 23:56:36 +0500

    use react hook form

--- src/App.js
+++ src/App.js
@@ -1,70 +1,71 @@
 import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import './App.css'
 
-const sendFormData = (formData) => {
-  console.log(formData)
-}
-
 function App() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [repeatPassword, setrepeatPassword] = useState('')
-
-  const [newError, setNewError] = useState(null)
-
-  let error = null
-
-  const onEmailChange = ({ target }) => {
-    setEmail(target.value)
-  }
-  const onPasswordChange = ({ target }) => setPassword(target.value)
-  const onRepeatPasswordChange = ({ target }) => setrepeatPassword(target.value)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+      repeatPassword: '',
+    },
+  })
 
   const [showPassword, setShowPassword] = useState(false)
+
+  const emailError = errors.email?.message
+  const passwordError = errors.password?.message
+
+  const emailProps = {
+    pattern: {
+      value: /\S+@\w+\W\w{2,6}/,
+      message:
+        'Ошибка. Введенный адрес почтового ящика не полный (дополните: .ru, .com и т.д)',
+    },
+  }
+
+  const passwordProps = {
+    minLength: {
+      value: 8,
+      message: 'Ошибка. В пароле должно быть не менее 8 символов',
+    },
+    pattern: {
+      value: /^[0-9A-Za-z_]+$/,
+      message:
+        'Ошибка. Пароль может содержать только латинские буквы, цифры и нижнее подчеркивание',
+    },
+  }
+
+  const onSubmit = (formData) => {
+    console.log(formData)
+  }
 
   const togglePassword = () => {
     setShowPassword((prevState) => !prevState)
   }
 
-  const onSubmit = (e) => {
-    e.preventDefault()
-    if (!/\S+@\w+\W\w{2,6}/gm.test(email)) {
-      error =
-        'Ошибка. Введенный адрес почтового ящика не полный (дополните: .ru, .com и т.д)'
-    } else if (!/^[0-9A-Za-z_]+$/gm.test(password)) {
-      error =
-        'Ошибка. Пароль может содержать только латинские буквы, цифры и нижнее подчеркивание'
-    } else if (password !== repeatPassword) {
-      error = 'Ошибка. Пароли не совпадают'
-    } else if (password.length < 8) {
-      error = 'Ошибка. В пароле должно быть не менее 8 символов'
-    } else {
-      sendFormData({ email, password, repeatPassword })
-    }
-
-    setNewError(error)
-  }
-
   return (
     <>
       <div className="container">
-        <form onSubmit={onSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <label>Почта</label>
           <input
             type="email"
             name="email"
-            value={email}
             required
-            onChange={onEmailChange}
+            {...register('email', emailProps)}
           />
           <label>Пароль</label>
-          <div className="test">
+          <div className="password">
             <input
               type={showPassword ? 'text' : 'password'}
               name="password"
-              value={password}
               required
-              onChange={onPasswordChange}
+              {...register('password', passwordProps)}
             />
             <button type="button" className="showPass" onClick={togglePassword}>
               {showPassword ? 'Скрыть ' : 'Показать '}пароль
@@ -75,25 +76,16 @@ function App() {
           <input
             type={showPassword ? 'text' : 'password'}
             name="repeatPassword"
-            value={repeatPassword}
             required
-            onChange={onRepeatPasswordChange}
+            {...register('repeatPassword', passwordProps)}
           />
-          <button
-            type="submit"
-            disabled={
-              email.length > 0 &&
-              password.length > 0 &&
-              repeatPassword.length > 0
-                ? false
-                : true
-            }
-          >
+          <button type="submit" disabled={!!emailError || !!passwordError}>
             Зарегистрироваться
           </button>
         </form>
       </div>
-      {newError && <div className="errorLabel">{newError}</div>}
+      {emailError && <div className="errorLabel">{emailError}</div>}
+      {passwordError && <div className="errorLabel">{passwordError}</div>}
     </>
   )
 }
